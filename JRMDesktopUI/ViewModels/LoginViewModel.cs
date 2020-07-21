f262f1ef2cd2f5adff6004978cdb2963d697ce7e
Josh Bentele <jbentele@outlook.com>
2020-07-21 17:46:24 -0500

    API to Login Form Wire Up
    Connects the login form to the authentication API endpoint (/token). Gets back bearer token or an exception if failed.

--- JRMDesktopUI/ViewModels/LoginViewModel.cs
+++ JRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using JRMDesktopUI.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,12 @@ namespace JRMDesktopUI.ViewModels
     {
 		private string _userName;
 		private string _password;
+		private IAPIHelper _apiHelper;
+
+		public LoginViewModel(IAPIHelper apiHelper)
+		{
+			_apiHelper = apiHelper;
+		}
 
 		public string UserName
 		{
@@ -48,8 +55,16 @@ namespace JRMDesktopUI.ViewModels
 			}
 		}
 
-		public void LogIn()
+		public async Task LogIn()
 		{
+			try
+			{
+				var result = await _apiHelper.Authenticate(UserName, Password);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
 		}
 	}
 }
